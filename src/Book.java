63fcfbaa0bab53481d9d567b6e6dc87187d48ae0
Timur <mirtimoha@gmail.com>
2023-01-31 12:05:36 +0500

    hw

---
+++ src/Book.java
@@ -0,0 +1,27 @@
+public class Book {
+    private final String title;
+    private final Author author;
+    private int releaseYear;
+
+    Book(String title, Author author, int releaseYear) {
+        this.title = title;
+        this.author = author;
+        this.releaseYear = releaseYear;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public int getReleaseYear() {
+        return releaseYear;
+    }
+
+    public void setReleaseYear(int releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -7,41 +7,4 @@ public class Main {
         whiteFang.setReleaseYear(2023);
     }
 }
-class Book {
-    private final String title;
-    private final Author author;
-    private int releaseYear;
 
-    Book(String title, Author author, int releaseYear) {
-        this.title = title;
-        this.author = author;
-        this.releaseYear = releaseYear;
-    }
-    public String getTitle() {
-        return title;
-    }
-    public Author getAuthor() {
-        return author;
-    }
-    public int getReleaseYear() {
-        return releaseYear;
-    }
-    public void setReleaseYear(int releaseYear) {
-        this.releaseYear = releaseYear;
-    }
-}
-class Author {
-    private final String firstName;
-    private final String lastName;
-
-    Author(String firstName, String lastName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
-    public String getFirstName() {
-        return firstName;
-    }
-    public String getLastName () {
-        return lastName;
-    }
-}
